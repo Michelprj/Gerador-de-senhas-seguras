32cef077dfabef1a64aebb080688526939cc38b2
Michel.P <michelpr1224@gmail.com>
2023-09-09 13:12:23 -0300

    Gerador finalizado

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+import { nanoid } from 'nanoid';
+import './style.css';
+import clipboardCopy from 'clipboard-copy';
+
+const passwordBrnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+
+passwordBrnEl.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  displayPasswordEl.innerHTML = randomPassword;
+  clipboardCopy(randomPassword);
+});
